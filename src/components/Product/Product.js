02aeb226e8391813fc4cffd085b6f1fa864e472b
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-08-07 22:05:50 +0600

    cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,9 +4,21 @@ import "./Product.css";
 const Product = (props) => {
   const { name, img, price, seller, ratings } = props.product;
   return (
-     <div className="product">
-        <img src={img} alt=""></img>
+    <div className="product">
+      <img src={img} alt=""></img>
+      <div className="product-info">
         <p className="product-name">{name}</p>
+        <p>Price: $ {price}</p>
+        <p>
+          <small>Seller: {seller}</small>
+        </p>
+        <p>
+          <small>Ratings: {ratings} Stars</small>
+        </p>
+        </div>
+        <button className="btn-cart">
+           <p>Add to Cart</p>
+        </button>
     </div>
   );
 };
